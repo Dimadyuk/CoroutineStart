d7aa0b571cab5592cd186d2e893411cfb3ab5164
Dimadyuk <dimadyuk88@gmail.com>
2022-08-25 14:23:38 +0200

    lesson 107 added Coroutines

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -5,7 +5,10 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.security.auth.callback.Callback
 import kotlin.concurrent.thread
 
@@ -21,50 +24,42 @@ class MainActivity : AppCompatActivity() {
         bindViews()
     }
 
-    private fun loadData() {
+    private suspend fun loadData() {
         Log.d("MainActivity", "Load started: $this")
         binding.tvLocation.isVisible = true
         binding.tvTemperature.isVisible = true
         binding.progressBar.isVisible = true
         binding.button.isEnabled = false
-        loadCity {
-            binding.tvLocation.text = it
-            loadTemperature(it) {
-                binding.tvTemperature.text = it.toString()
-                binding.progressBar.isVisible = false
-                binding.button.isEnabled = true
-                Log.d("MainActivity", "Load finished: $this")
-            }
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemperature(city)
+        binding.tvTemperature.text = temp.toString()
+        binding.progressBar.isVisible = false
+        binding.button.isEnabled = true
+        Log.d("MainActivity", "Load finished: $this")
 
-        }
 
     }
 
-    private fun loadCity(callback: (String) -> Unit) {
-        thread {
-            Thread.sleep(2000)
-            runOnUiThread {
-                callback.invoke("Minsk")
-            }
-        }
+
+    private suspend fun loadCity(): String {
+
+        delay(2000)
+        return "Minsk"
     }
 
-    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+    private suspend fun loadTemperature(city: String): Int {
 
-        thread {
-            runOnUiThread {
-                Toast.makeText(
-                    this,
-                    "Loading temperature for city: $city",
-                    Toast.LENGTH_SHORT
-                ).show()
 
-                Thread.sleep(2000)
-                runOnUiThread {
-                    callback.invoke(17)
-                }
-            }
-        }
+        Toast.makeText(
+            this,
+            "Loading temperature for city: $city",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        delay(2000)
+
+        return 17
 
     }
 
@@ -75,7 +70,9 @@ class MainActivity : AppCompatActivity() {
             progressBar.isVisible = false
 
             button.setOnClickListener {
-                loadData()
+                lifecycleScope.launch {
+                    loadData()
+                }
             }
         }
     }
