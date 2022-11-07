089f03dfa9ae241976c40c934a1a5bca3d2c045d
Dimadyuk <dimadyuk88@gmail.com>
2022-11-07 16:13:17 +0100

    lesson 250 work with coroutineScope

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.coroutinestart.databinding.ActivityMainBinding
 import kotlinx.coroutines.async
@@ -18,140 +19,16 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        bindViews()
-    }
 
-    private suspend fun loadData() {
-        Log.d("MainActivity", "Load started: $this")
-        binding.tvLocation.isVisible = true
-        binding.tvTemperature.isVisible = true
-        binding.progressBar.isVisible = true
-        binding.button.isEnabled = false
-        val city = loadCity()
-
-        binding.tvLocation.text = city
-        val temp = loadTemperature()
-
-        binding.tvTemperature.text = temp.toString()
-        binding.progressBar.isVisible = false
-        binding.button.isEnabled = true
-        Log.d("MainActivity", "Load finished: $this")
-    }
-
-    private fun loadWithoutCoroutine(step: Int = 0, obj: Any? = null) {
-        when (step) {
-            0 -> {
-                Log.d("MainActivity", "Load started: $this")
-                binding.tvLocation.isVisible = true
-                binding.tvTemperature.isVisible = true
-                binding.progressBar.isVisible = true
-                binding.button.isEnabled = false
-                binding.tvLocation.text = ""
-                binding.tvTemperature.text = ""
-
-                loadCityWithoutCoroutine {
-                    loadWithoutCoroutine(1, it)
-                }
-
-            }
-            1 -> {
-                val city = obj as String
-                binding.tvLocation.text = city
-                loadTemperatureWithoutCoroutine(city) {
-                    loadWithoutCoroutine(2, it)
-                }
-            }
-            2 -> {
-                val temp = obj as Int
-                binding.tvTemperature.text = temp.toString()
-                binding.progressBar.isVisible = false
-                binding.button.isEnabled = true
-                Log.d("MainActivity", "Load finished: $this")
-            }
-
-        }
+        viewModel.method()
 
     }
 
-    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
-        Handler(Looper.getMainLooper()).postDelayed({
-            callback.invoke("Kiev")
-        }, 2000)
-    }
-
-    private suspend fun loadCity(): String {
-
-        delay(2000)
-        return "Minsk"
-    }
-
-    private fun loadTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
-        Handler(Looper.getMainLooper()).post {
-            Toast.makeText(
-                this,
-                "Loading temperature for city: $city",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            callback.invoke(17)
-        }, 2000)
-
-    }
-
-    private suspend fun loadTemperature(): Int {
-
-
-//        Toast.makeText(
-//            this,
-//            "Loading temperature for city: $city",
-//            Toast.LENGTH_SHORT
-//        ).show()
-
-        delay(2000)
-
-        return 17
-
-    }
-
-    private fun bindViews() {
-        with(binding) {
-            tvLocation.isVisible = false
-            tvTemperature.isVisible = false
-            progressBar.isVisible = false
-
-            button.setOnClickListener {
-                progressBar.isVisible = true
-                button.isEnabled = false
-                val deferredCity = lifecycleScope.async {
-                    loadCity()
-                }
-                val deferredTemp = lifecycleScope.async {
-                    loadTemperature()
-                }
-                lifecycleScope.launch {
-                    val city = deferredCity.await()
-                    val temp = deferredTemp.await()
-                    tvLocation.isVisible = true
-                    tvLocation.text = city
-                    tvTemperature.isVisible = true
-                    tvTemperature.text = temp.toString()
-
-                    Toast.makeText(
-                        this@MainActivity,
-                        "city: $city - temp: $temp",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    progressBar.isVisible = false
-                    button.isEnabled = true
-                }
-                //loadWithoutCoroutine()
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/coroutinestart/MainViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.coroutinestart
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import java.lang.Exception
+import java.lang.RuntimeException
+
+
+class MainViewModel : ViewModel() {
+
+    private val parentJob = Job()
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.d(LOG_TAG, "Exception caught: $throwable")
+    }
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob + exceptionHandler)
+
+
+    fun method() {
+        val childJob1 = coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "first coroutine finished")
+        }
+
+        val childJob2 = coroutineScope.launch {
+            delay(2000)
+            Log.d(LOG_TAG, "second coroutine finished")
+        }
+
+        val childJob3 = coroutineScope.launch {
+            delay(1000)
+            error()
+            Log.d(LOG_TAG, "third coroutine finished")
+        }
+    }
+
+    private fun error() {
+        throw RuntimeException()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+    companion object {
+        private const val LOG_TAG = "MainViewModel"
+    }
+}
